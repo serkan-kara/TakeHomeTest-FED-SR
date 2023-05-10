600115be9bf01aada4cff59c0d85e15a26877b6c
Serkan Kara <b.serkankara@gmail.com>
2023-05-10 09:27:41 +0300

    navigation, search input and avatar updates

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,14 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/layout.tsx": [
-      "/Users/serkankara/Desktop/TakeHomeTest-FED-SR/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/page": [
-      "/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/globals.css",
-      "/Users/serkankara/Desktop/TakeHomeTest-FED-SR/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
-    ]
-  }
-}
+self.__RSC_CSS_MANIFEST={"cssImports":{"/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/layout.tsx":["/Users/serkankara/Desktop/TakeHomeTest-FED-SR/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}","/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/globals.css"]},"cssModules":{"/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/page":["/Users/serkankara/Desktop/TakeHomeTest-FED-SR/src/app/globals.css","/Users/serkankara/Desktop/TakeHomeTest-FED-SR/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"]}}
